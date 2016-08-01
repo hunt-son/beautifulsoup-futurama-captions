902a1b677320391005be47d9e7efaf41805fe31c
Hunter Sonn <hsonn@umich.edu>
2016-08-01 11:03:03 -0400

    Everything is fine except character encoding still needs to be solved

--- bs-futurama-captions.py
+++ bs-futurama-captions.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
-soup = BeautifulSoup('http://www.aeromental.net/2010/01/03/futurama-intros-all-opening-subtitles-tag-lines/', 'html.parser').find('id=content').findall('em')
+import requests
+html = requests.get("http://www.aeromental.net/2010/01/03/futurama-intros-all-opening-subtitles-tag-lines/")
 
-text = open('futurama-captions.txt')
-text.write(soup)
-text.close()
+# html.encode('utf8')
+
+with open('futurama-captions.txt','w') as text:
+ 	soup = BeautifulSoup(html.text, 'html.parser')
+ 	soup = soup.find('body')
+ 	text.write(soup.prettify())
+
+
+# for li in soup.find_all('li'):
+# 	print(li)
+# print (soup.prettify)
+# text = open('futurama-captions.txt')
+# text.write(soup)
+# text.close()
